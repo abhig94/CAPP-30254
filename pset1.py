a00e4d1378b76a76a98e54ad6e77d12117e8ce18
Abhi Gupta <abhig94@gmail.com>
2016-04-04 16:53:36 -0500

    Pset 1, final
    Completed Problem Set 1.

--- pset1.py
+++ pset1.py
@@ -38,13 +38,21 @@ for col in data.columns:
         continue
 
 print(summary_stats)
-# need some summary plots. May make sense to change Gender into a numeric for
-# summary stats
+
+fig =  data.State.value_counts().plot(kind = 'bar')
+fig.get_figure().savefig('States.png')
+fig =  data.Gender.value_counts().plot(kind = 'bar')
+fig.get_figure().savefig('Gender.png')
+fig =  data.Graduated.value_counts().plot(kind = 'bar')
+fig.get_figure().savefig('Graduated.png')
+fig = data.hist(layout=(2,2))
+fig[0][1].get_figure().savefig('Hists.png')
+
 
 
 ## inferring missing genders
 missing_genders = np.where(data['Gender'].isnull())[0]
-data_2 = data.copy()
+data_2 = data.copy()    
 for i in missing_genders:
     name = data_2.ix[i,'First_name']
     web_req = requests.get("http://api.genderize.io?name[0]="+name)
@@ -59,14 +67,10 @@ data_2.to_csv('gender_imputed.csv')
         
         
     
-## replacing missing values with mean/mode as appropriate  
+## replacing missing values with mean
     
 data_3 = data_2.copy()
-for col in ['State']:
-    ind = pd.isnull(data_3[col])
-    fill_val = data_3[col].mode()[0]
-    data_3.ix[ind,col] = fill_val
-    
+   
 for col in ['Age','GPA','Days_missed']:
     ind = pd.isnull(data_3[col])
     fill_val = data_3[col].mean()
@@ -76,18 +80,10 @@ data_3.to_csv('method_A.csv')
     
     
 
-# replacing missing vals with conditional mean/mode
+## replacing missing vals with conditional mean
 data_4 = data_2.copy()
 grouped = data_4.groupby('Graduated')
-conditional_modes = grouped.agg(lambda x:x.value_counts().index[0])
 conditional_means = grouped.mean()
-# We weren't asked to do state, but oh well. 
-for col in ['State']:
-    ind = pd.isnull(data_4[col])
-    fill_vals_func = lambda x: conditional_modes.ix[x['Graduated'],col]
-    fill_vals = fill_vals_func(data_4.ix[ind,:])
-    fill_vals.index = data_4.ix[ind,:].index
-    data_4.ix[ind,col] = fill_vals
     
 for col in ['Age','GPA','Days_missed']:
     ind = pd.isnull(data_4[col])
@@ -98,32 +94,29 @@ for col in ['Age','GPA','Days_missed']:
 
 data_4.to_csv('method_B.csv')
 
-# replacing missing vals with conditional mean/mode
 
-def impute_func(x):
-    try:
-        return x.mean()
-    except:
-        return x.value_counts().index[0]
-        
-data_5 = data_2.copy()        
-grouped = data_5.groupby(['Graduated','Gender'])
+
+## replacing missing vals with finer conditional means     
+data_5 = data_2.copy()
+
+# first, impute missing states
+grouped = data_5.groupby('Graduated')
 conditional_modes = grouped.agg(lambda x:x.value_counts().index[0])
 for col in ['State']:
     ind = pd.isnull(data_5[col])
-    fill_vals_func = lambda x: conditional_modes.ix[x['Graduated'],x['Gender']]['State']
+    fill_vals_func = lambda x: conditional_modes.ix[x['Graduated'],col]
     fill_vals = fill_vals_func(data_5.ix[ind,:])
     fill_vals.index = data_5.ix[ind,:].index
     data_5.ix[ind,col] = fill_vals
     
-    
-for col in ['State']:
-    ind = pd.isnull(data_5[col])
-    fill_val = data_3[col].mode()[0]
-    data_5.ix[ind,col] = fill_val
-    
+# find conditional 
 grouped = data_5.groupby(['Graduated','Gender','State'])
-
+conditional_means = grouped.mean()
+for col in ['Age','GPA','Days_missed']:
+    ind = pd.isnull(data_5[col])
+    for row in np.where(ind==True)[0]:
+        fill_val = conditional_means.ix[data_5.ix[row,'Graduated'],
+                  data_5.ix[row,'Gender'],data_5.ix[row,'State']]
+        data_5.ix[row,col] = fill_val[col]
 
 data_5.to_csv('method_C.csv')
-data_5.ix[10:20,'State']
