3f57d85cf449bbf24e1990ff9ceada7518cd9f2b
dtroberts94 <danielroberts@uchicago.edu>
2016-05-15 16:31:48 -0500

    blah

--- Project/Pipeline/first_pass.py
+++ Project/Pipeline/first_pass.py
@@ -6,6 +6,6 @@ data = replace_value(data,'regionwb',np.NaN,'Non_OECD_Rich')
 data = replace_value(data, list(data), np.NaN, 0)
 data = get_q_24(data)
 y_name = 'q24'
-x_names = list(data).remove('q24')
+x_names = list(data).remove('q24','wgt')
 y = data[y_name]
 x = data[x_names]
