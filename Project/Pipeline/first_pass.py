4a4687836744d7f4c15fa606ec16766d7fed7296
dtroberts94 <danielroberts@uchicago.edu>
2016-05-15 16:27:01 -0500

    blah

---
+++ Project/Pipeline/first_pass.py
@@ -0,0 +1,11 @@
+from handleData import *
+from pipe import *
+filename = 'micro_world.csv'
+data = readcsv(filename)
+data = replace_value(data,'regionwb',np.NaN,'Non_OECD_Rich')
+data = replace_value(data, list(data), np.NaN, 0)
+data = get_q_24(data)
+y_name = 'q24'
+x_names = list(data).remove('q24')
+y = data[y_name]
+x = data[x_names]
