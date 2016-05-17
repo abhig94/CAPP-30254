d13065a2aa94064674774f08c5af086a459acfba
Michael Fosco <mfosco55@gmail.com>
2016-05-17 15:12:39 -0500

    eh

--- Project/Pipeline/mike.py
+++ Project/Pipeline/mike.py
@@ -4,12 +4,8 @@
 @author: Mike
 '''
 
+'''
 def get_q_24(data):
-	'''
-	@return: process data to turn q_24 into binary with only its valid rows
-	@input: data			The data to process
-	This function depends on replace_value.
-	'''
 	df = data[data.q24 != 5]
 	df = data[data.q24 != 6]
 
@@ -17,6 +13,7 @@ def get_q_24(data):
 	df = replace_value(df, ['q24'], 2, 0)
 	df = replace_value(df, ['q24'], 3, 1)
 	return replace_value(df, ['q24'], 4, 1)
+'''
 
 '''
 A value is trying to be set on a copy of a slice from a DataFrame.
@@ -24,3 +21,5 @@ Try using .loc[row_indexer,col_indexer] = value instead
 
 See the caveats in the documentation: http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-view-versus-copy
 '''
+
+
