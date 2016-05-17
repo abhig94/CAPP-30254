7cebfa52d74ed0084eaa54d920a659edb523a007
Abhi Gupta <abhig94@gmail.com>
2016-05-17 16:08:49 -0500

    ch ch ch changes

--- Project/Pipeline/first_run.py
+++ Project/Pipeline/first_run.py
@@ -8,11 +8,11 @@ simple_modelSVC = {'model': svm.LinearSVC}
 simple_modelLR = {'model': LogisticRegression}
 simple_modelRF  = {'model': RandomForestClassifier}
 simple_modelNB  = {'model': GaussianNB}
-simple_modelDT  = {'model': DecisionTreeClassifier, 'max_depth': [50], 'max_features': ['sqrt']
+simple_modelDT  = {'model': DecisionTreeClassifier, 'max_depth': [50], 'max_features': ['sqrt'],
 			'min_samples_split': [50]}
 simple_modelDTR = {'model': DecisionTreeRegressor}
 
-modelList = [simple_modelDT]
+modelList = [simple_modelLR]
 
 ###########################################################
 os.chdir('..')
@@ -22,5 +22,5 @@ os.chdir('Data')
 os.chdir('Output')
 x = readcsv('x.csv',index_col = 0)
 y = readcsv('y.csv',index_col = 0)
-results = pipeLine(y,x modelList, 5)
+results = pipeLine(y,x, modelList, 5)
 write_results_to_file('first_results.csv', results)
