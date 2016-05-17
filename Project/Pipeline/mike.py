ce7edd9d96cd464192d3e5b67a11c46ef2516a44
Michael Fosco <mfosco55@gmail.com>
2016-05-17 15:31:21 -0500

    stopped

--- Project/Pipeline/mike.py
+++ Project/Pipeline/mike.py
@@ -22,4 +22,25 @@ Try using .loc[row_indexer,col_indexer] = value instead
 See the caveats in the documentation: http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-view-versus-copy
 '''
 
+import os
+from os import listdir
+from os.path import isfile, join
+import subprocess
+
+def root_path():
+    return os.path.abspath(os.sep)
+
+
+def getAllFiles(path):
+	files = [f for f in listdir(path) if isfile(join(path, f))]
+	return files
+
+
+def save_to_dropbox(df):
+	path = os.getcwd()
+
+	fOne = getAllFiles(path)
+	if 'Users' in fOne:
+		path += 'Users'
+
 
