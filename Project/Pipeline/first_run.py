9e18a6f88aba68b9c7c53bc0127cb03affe8164c
dtroberts94 <danielroberts@uchicago.edu>
2016-05-22 15:53:29 -0500

    updates

--- Project/Pipeline/first_run.py
+++ Project/Pipeline/first_run.py
@@ -1,5 +1,5 @@
 from handleData import *
-from abhi_pipeline import *
+from pipe import *
 
 
 ############################################################
@@ -17,7 +17,6 @@ modelDT  = {'model': DecisionTreeClassifier, 'criterion': ['gini', 'entropy'], '
 
 
 modelList = [modelDT, simple_modelRF, simple_modelDTR, simple_modelNB, simple_modelLR, simple_modelSVC]
-
 ###########################################################
 os.chdir('..')
 os.chdir('..')
@@ -26,6 +25,9 @@ os.chdir('Data')
 os.chdir('Output')
 x = readcsv('x_nodiscrete.csv',index_col = 0)
 y = readcsv('y.csv',index_col = 0)
+weights = readcsv('weights.csv',index_col = 0)
 to_discretize =  ['pop_adult','age']
 results = clf_loop_reloaded(x,y,5,modelList,to_discretize,10)#pipeLine(y,x, modelList, 5)
 write_results_to_file('first_results.csv', results)
+weight_results = clf_loop_reloaded_weights(x,y,5,[simple_modelDT],to_discretize,10,weights)
+write_results_to_file('first_weight_results.csv', weight_results)
