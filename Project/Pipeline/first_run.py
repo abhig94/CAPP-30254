d2a9c51f0c2cd98b55eab9da9d7ba89402cc269e
Michael Fosco <mfosco55@gmail.com>
2016-05-25 15:59:38 -0500

    bug

--- Project/Pipeline/first_run.py
+++ Project/Pipeline/first_run.py
@@ -25,7 +25,7 @@ modelET  = {'model': ExtraTreesClassifier, 'n_estimators': [25, 50, 100], 'crite
             'max_features': ['sqrt', 'log2'], 'max_depth': depth,
             'bootstrap': [True, False], 'n_jobs':[cores]}
 modelLR = {'model': LogisticRegression, 'solver': ['liblinear'], 'C' : [.01, .1, .5, 1],#, 5, 10, 25],
-          'class_weight': ['balanced', None], 'n_jobs' : [cores],
+          'class_weight': ['balanced', None], 'n_jobs' : [cores]}
 
 
 modelList = [modelDT, modelRF, modelAB, modelET, simple_modelDTR, simple_modelNB, smodelLR, simple_modelSVC]
