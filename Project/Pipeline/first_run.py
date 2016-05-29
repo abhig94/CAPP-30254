818eeb34eb9bcf75f88a6a8472340cee9ea9ee21
Michael Fosco <mfosco55@gmail.com>
2016-05-29 14:24:14 -0500

    the almighty script

--- Project/Pipeline/first_run.py
+++ Project/Pipeline/first_run.py
@@ -1,25 +1,29 @@
 from handleData import *
 from pipe import *
 
+import sys
+
 
 ############################################################
 ''' Model Defintions '''
 cpus = mp.cpu_count()
-cores = max(cpus/2-1,1)
-depth = [10, 20, 50]
+cores = max(cpus-1,1)
+depth = [10, 20, 50, 100]
+###########################################################
 simple_modelSVC = {'model': svm.LinearSVC}
 simple_modelLR = {'model': LogisticRegression}
 simple_modelRF  = {'model': RandomForestClassifier}
-simple_modelNB  = {'model': GaussianNB}
 simple_modelDT  = {'model': DecisionTreeClassifier, 'max_depth': [50], 'max_features': ['sqrt'],
 			'min_samples_split': [50]}
 simple_modelDTR = {'model': DecisionTreeRegressor}
 
-modelDT  = {'model': DecisionTreeClassifier, 'criterion': ['gini', 'entropy'], 'max_depth': [50, 100], #1, 5, 10,20,
+########################################################
+
+modelDT  = {'model': DecisionTreeClassifier, 'criterion': ['gini', 'entropy'], 'max_depth': depth, #1, 5, 10,20,
             'max_features': ['sqrt','log2'],'min_samples_split': [2, 5, 10, 20, 50]}
 modelRF  = {'model': RandomForestClassifier, 'n_estimators': [25, 50, 100], 'criterion': ['gini', 'entropy'],
-            'max_features': ['sqrt', 'log2'], 'max_depth': depth, 'min_samples_split': [20, 50], #min sample split also had 2, 5, 10
-            'bootstrap': [True], 'n_jobs':[3]} #bootstrap also had False
+            'max_features': ['sqrt', 'log2'], 'max_depth': depth, 'min_samples_split': [2, 5, 10, 20, 50], #min sample split also had 2, 5, 10
+            'bootstrap': [True], 'n_jobs':[cores]} #bootstrap also had False
 modelAB  = {'model': AdaBoostClassifier, 'algorithm': ['SAMME', 'SAMME.R'], 'n_estimators': [25, 50]}#, 200]}
 modelET  = {'model': ExtraTreesClassifier, 'n_estimators': [25, 50, 100], 'criterion': ['gini', 'entropy'],
             'max_features': ['sqrt', 'log2'], 'max_depth': depth,
@@ -27,10 +31,14 @@ modelET  = {'model': ExtraTreesClassifier, 'n_estimators': [25, 50, 100], 'crite
 modelLR = {'model': LogisticRegression, 'solver': ['liblinear'], 'C' : [.01, .1, .5, 1],#, 5, 10, 25],
           'class_weight': ['balanced', None], 'n_jobs' : [cores],
           'tol' : [1e-5, 1e-3, 1], 'penalty': ['l1', 'l2']}
+modelNB  = {'model': GaussianNB}     
+modelDTR = {'model': DecisionTreeRegressor, 'max_features': ['sqrt', 'log2'], 'max_depth': depth,
+            'min_samples_split': [2, 5, 10, 20, 50]}     
 
+modelList = [modelDT, modelRF, modelAB, modelET, modelLR, modelNB, modelDTR]
 
-modelList = [modelDT, modelRF, modelAB, modelET, simple_modelDTR, simple_modelNB, modelLR, simple_modelSVC]
-modelList2 = [simple_modelDT, simple_modelLR, simple_modelDTR]
+#modelList = [modelDT, modelRF, modelAB, modelET, simple_modelDTR, simple_modelNB, modelLR, simple_modelSVC]
+#modelList2 = [simple_modelDT, simple_modelLR, simple_modelDTR]
 ###########################################################
 
 os.chdir('..')
@@ -38,6 +46,7 @@ os.chdir('..')
 os.chdir('..')
 os.chdir('Data')
 os.chdir('Output')
+
 
 x = readcsv('x_nodiscrete.csv',index_col = 0)
 y = readcsv('y.csv',index_col = 0)
@@ -63,7 +72,6 @@ best_clfs.to_csv('best_clfs.csv')
 comparison = compare_clf_across_metric(all_results,'AUC')
 comparison.to_csv('comparison_of_clfs.csv')
 
-'''
 # using the model weights
 all_results = pd.read_csv('modelList_weight_results.csv')
 all_results = clean_results(all_results,criteria)
